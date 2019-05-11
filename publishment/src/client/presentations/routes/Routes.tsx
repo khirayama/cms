079571aff181778b121bd34c11a1b37460afcb76
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-05-12 08:13:51 +0900

    Use PrivateRoute

--- publishment/src/client/presentations/routes/Routes.tsx
+++ publishment/src/client/presentations/routes/Routes.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
 import { Route, Switch } from 'react-router-dom';
 
+import { PrivateRoute } from '../../../client/presentations/routes/PrivateRoute';
 import { Home } from '../../../client/presentations/pages/Home';
 import { SignUp } from '../../../client/presentations/pages/SignUp';
 import { SignIn } from '../../../client/presentations/pages/SignIn';
@@ -16,9 +17,24 @@ export function Routes(props: Props) {
     <Switch>
       <Route exact path={`${props.baseUrl}/signup`} render={() => <SignUp baseUrl={props.baseUrl} />} />
       <Route exact path={`${props.baseUrl}/signin`} render={() => <SignIn baseUrl={props.baseUrl} />} />
-      <Route exact path={`${props.baseUrl}/`} render={() => <Home baseUrl={props.baseUrl} />} />
-      <Route exact path={`${props.baseUrl}/about`} render={() => <About baseUrl={props.baseUrl} />} />
-      <Route exact path={`${props.baseUrl}/users`} render={() => <Users baseUrl={props.baseUrl} />} />
+      <PrivateRoute
+        baseUrl={props.baseUrl}
+        exact
+        path={`${props.baseUrl}/`}
+        render={() => <Home baseUrl={props.baseUrl} />}
+      />
+      <PrivateRoute
+        baseUrl={props.baseUrl}
+        exact
+        path={`${props.baseUrl}/about`}
+        render={() => <About baseUrl={props.baseUrl} />}
+      />
+      <PrivateRoute
+        baseUrl={props.baseUrl}
+        exact
+        path={`${props.baseUrl}/users`}
+        render={() => <Users baseUrl={props.baseUrl} />}
+      />
     </Switch>
   );
 }
