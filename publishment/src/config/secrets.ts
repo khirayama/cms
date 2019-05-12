c540a6cd0e85b62f776035ceb25d771ee14cc108
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-05-13 08:39:03 +0900

    Add a secret file for dev

---
+++ publishment/src/config/secrets.ts
@@ -0,0 +1,4 @@
+// FYI: These values are secrets. Please change these values for production.
+export const SESSION_SECRET = 'asdfghjkl';
+export const MONGODB_URI = 'mongodb://localhost:27017';
+export const MONGODB_URI_DATABASE_NAME = 'publishment';
