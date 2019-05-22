44182e0771a3e5492d9b065c48822e77bdba20e1
Kotaro Hirayama <just.do.it.my.life@gmail.com>
2019-05-22 12:03:00 +0900

    Create Role model

---
+++ publishment/src/models/Role.ts
@@ -0,0 +1,14 @@
+import mongoose from 'mongoose';
+
+export type RoleDocument = mongoose.Document & {
+  name: string;
+};
+
+const roleSchema = new mongoose.Schema(
+  {
+    name: String,
+  },
+  { timestamps: true },
+);
+
+export const Role = mongoose.model<RoleDocument>('Role', roleSchema);
